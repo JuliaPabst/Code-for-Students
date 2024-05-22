fd8572d4d26a2ca96f691ed9a1e2f4866ee88b9e
Christoph Gartner <gartner-christoph@gmx.at>
2024-05-22 16:07:19 +0200

    added detail-view, edit-profile, changed home component

---
+++ Frontend/src/app/detail-view/detail-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-view.component.html',
+  styleUrl: './detail-view.component.css'
+})
+export class DetailViewComponent {
+
+}
--- Models/User.ts
+++ Models/User.ts
@@ -1,4 +1,5 @@
 class User {
+    imageRef: string;
     name: string;
     description: string;
     status: string;
@@ -6,6 +7,7 @@ class User {
     totalPosts: number;
     totalComments: number;
     constructor() {
+        this.imageRef = "";
         this.name = "";
         this.description = "";
         this.status = "";
