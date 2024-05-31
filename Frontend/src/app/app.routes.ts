a6d75260ac41707d45649fbd38b28b46202ed956
Jules <testadmin@Julias-MBP-2.home>
2024-05-31 10:48:47 +0200

    add profile route

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {'path': '', component: HomeComponent},
     {'path': 'register', component: RegisterComponent},
-    {'path': 'login', component: LoginComponent}
+    {'path': 'login', component: LoginComponent},
+    {'path': 'profile', component: ProfileComponent}
 ];
