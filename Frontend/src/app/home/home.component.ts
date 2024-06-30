63b539ec714dd2f0a8ce88beda8a377b106a501a
Christoph Gartner <gartner-christoph@gmx.at>
2024-06-30 18:03:09 +0200

    fixed connection from home component to backend

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -3,11 +3,11 @@ import { Component, OnInit, inject } from '@angular/core';
 import { NgIf } from '@angular/common';
 
 @Component({
-selector: 'app-home',
-standalone: true,
-imports: [HttpClientModule, NgIf],
-templateUrl: './home.component.html',
-styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    imports: [HttpClientModule, NgIf],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
 
@@ -18,32 +18,31 @@ comments_count : number = 0;
 show_comments : boolean = false;
 
 ngOnInit(): void {
-this.fetchPosts();
-this.fetchComments();
+    this.fetchPosts();
+    this.fetchComments();
 } 
 fetchPosts(){
-this.httpClient
-/* .get('https://jsonplaceholder.typicode.com/posts') */
-.get('http://localhost:5000/posts')
-.subscribe((posts : any)=>{
-console.log(posts);
-this.posts = posts;
-})
+    this.httpClient
+    .get('http://localhost:3000/posts')
+    .subscribe((posts : any)=>{
+        console.log(posts);
+        this.posts = posts;
+    })
 }
 fetchComments() {
-this.httpClient
-.get('http://localhost:5000/comments')
-.subscribe((comments: any) => {
-this.comments = comments;
-this.posts.forEach(post => {
-const postComments = this.comments.filter(comment => comment.postId === post.id);
-post.comments_count = postComments.length;
-post.showComments = false; 
-});
-})
+    this.httpClient
+    .get('http://localhost:3000/comments')
+    .subscribe((comments: any) => {
+        this.comments = comments;
+        this.posts.forEach(post => {
+            const postComments = this.comments.filter(comment => comment.postId === post.id);
+            post.comments_count = postComments.length;
+            post.showComments = false; 
+        });
+    })
 }
 toggleComments(post: any) {
-post.showComments = !post.showComments;
-}
+    post.showComments = !post.showComments;
+    }
 }
 
